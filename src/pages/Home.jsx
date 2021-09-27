5b820ed778ad24881641da8723bfb6f0c3260c0f
Solomon Talemwa <adamtblack96@gmail.com>
2021-09-27 13:28:34 +0300

    Add announcement banner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import AnnouncementBanner from '../components/AnnouncementBanner'
 
 const Home = () => {
     return (
         <div>
+            <AnnouncementBanner/>
             <Navbar/>
         </div>
     )
