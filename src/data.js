fcb231fdbbe095d55c094c425482ff61eb0ef118
Solomon Talemwa <adamtblack96@gmail.com>
2021-10-21 19:11:48 +0300

    add carousel component to home section

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const SliderItems = [
+  {
+    id: 1,
+    img: "https://bit.ly/3aWdqrA",
+    title: "NEVER SAY WAIT",
+    description: "OUR BEST COLLECTIONS YET. 25% OFF ON OUR NEW ARRIVALS",
+    bg: "eecfee",
+  },
+  {
+    id: 2,
+    img: "https://bit.ly/3aWdqrA",
+    title: "IT IS NOW",
+    description: "OUR BEST COLLECTIONS YET. 25% OFF ON OUR NEW ARRIVALS",
+    bg: "dad0c1",
+  },
+  {
+    id: 3,
+    img: "https://bit.ly/3aWdqrA",
+    title: "THIS CAN BE YOURS",
+    description: "OUR BEST COLLECTIONS YET. 25% OFF ON OUR NEW ARRIVALS",
+    bg: "c1ccda",
+  },
+];
