5c9eaeed7fc66a0b6a04952d83205aff6c406e77
Solomon Talemwa <adamtblack96@gmail.com>
2021-12-12 01:14:58 +0300

    add product list section

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -109,7 +109,7 @@ const Slider = () => {
             <Wrapper slideIndex={slideIndex}>
             {SliderItems.map((item) => (
 
-                <Slide bg={item.bg}>
+                <Slide bg={item.bg} key={item.id}>
                 <ImgContainer>
                 <Image src={item.img}/>
                 </ImgContainer>
